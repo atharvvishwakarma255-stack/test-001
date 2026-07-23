1e10302dd7a38224620fdc3fb597faf852c172c0
AniketVishwakarma7575 <aniketv123456ishwakarma@gmail.com>
2026-07-23 16:50:15 +0530

    test 001

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello from Server 1");
+});
+
+app.get("/test", (req, res) => {
+    res.json({
+        success: true,
+        server: "Server 1",
+        message: "Test route is working"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server 1 is running on http://localhost:${PORT}`);
+});
