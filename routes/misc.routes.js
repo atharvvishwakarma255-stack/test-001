d62d52c847b36086fb76e43b9011cc64eb75355a
AniketVishwakarma7575 <aniketv123456ishwakarma@gmail.com>
2026-07-24 15:20:10 +0530

    test 006

--- routes/misc.routes.js
+++ routes/misc.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { home, dashboard, testRoute, test2Route } = require("../controllers/misc.controller");
+const { home, dashboard, testRoute, test2Route, test3Route } = require("../controllers/misc.controller");
 
 router.get("/one", home);
 router.get("/dashboard", dashboard);
 router.get("/hello1", testRoute);
 router.get("/test2", test2Route);
-
+router.get("/test3", test3Route);
 
 module.exports = router;
