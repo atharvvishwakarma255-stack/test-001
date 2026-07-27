bb414ceedd200f1f81fc3cdb90263600cc17828d
AniketVishwakarma7575 <aniketv123456ishwakarma@gmail.com>
2026-07-27 10:02:41 +0530

    remove home1, home2 routes

--- controllers/misc.controller.js
+++ controllers/misc.controller.js
@@ -1,13 +1,3 @@
-function home(req, res) {
-  res.send("Hello from Server 1");
-}
-
-
-function home2(req, res) {
-  res.send('hello from home 2 !')
-}
-
-
 
 function dashboard(req, res) {
   res.send("Hello from Server 1");
@@ -25,4 +15,4 @@ function test3Route(req, res) {
   res.json({ success: true, server: "Server 1", message: "Test route 3 is working" });
 }
 
-module.exports = { home, home2, dashboard, testRoute, test2Route, test3Route };
+module.exports = { dashboard, testRoute, test2Route, test3Route };
--- routes/misc.routes.js
+++ routes/misc.routes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { home, home2, dashboard, testRoute, test2Route, test3Route } = require("../controllers/misc.controller");
+const { dashboard, testRoute, test2Route, test3Route } = require("../controllers/misc.controller");
 
-router.get("/one", home);
-router.get('/home', home2)
+
 router.get("/dashboard", dashboard);
 router.get("/hello1", testRoute);
 router.get("/test2", test2Route);
