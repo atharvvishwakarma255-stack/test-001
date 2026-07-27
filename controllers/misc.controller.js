ae045e606a4b03f84514df14f57ed2854fd2c13d
AniketVishwakarma7575 <aniketv123456ishwakarma@gmail.com>
2026-07-27 14:50:41 +0530

    remove dashboard, test2 routes

--- controllers/misc.controller.js
+++ controllers/misc.controller.js
@@ -1,11 +1,2 @@
 
-function dashboard(req, res) {
-  res.send("Hello from Server 1");
-}
-
-function test2Route(req, res) {
-  res.json({ success: true, server: "Server 1", message: "Test route 2 is working" });
-}
-
-
-module.exports = { dashboard, test2Route };
+module.exports = {};
--- routes/misc.routes.js
+++ routes/misc.routes.js
@@ -1,10 +1,4 @@
 const express = require("express");
 const router = express.Router();
-const { dashboard, test2Route } = require("../controllers/misc.controller");
-
-
-router.get("/dashboard", dashboard);
-router.get("/test2", test2Route);
-
 
 module.exports = router;
