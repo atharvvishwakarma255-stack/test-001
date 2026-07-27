bfab27b54378851c703ef4ff9eb2a27686cbfaea
AniketVishwakarma7575 <aniketv123456ishwakarma@gmail.com>
2026-07-27 14:53:54 +0530

    remove document status and template placeholders routes

--- routes/documents.routes.js
+++ routes/documents.routes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const requireBusiness = require("../middlewares/requireBusiness");
-const { sendDocument, getDocumentStatus } = require("../controllers/documents.controller");
+const { sendDocument } = require("../controllers/documents.controller");
 
 router.post("/api/v1/document/sends", requireBusiness, sendDocument);
-router.get("/api/v1/document/status/:recipientDocumentId", requireBusiness, getDocumentStatus);
 
 module.exports = router;
--- routes/templates.routes.js
+++ routes/templates.routes.js
@@ -1,13 +1,4 @@
 const express = require("express");
 const router = express.Router();
-const { authenticate, authorize } = require("../middlewares/auth");
-const { getTemplatePlaceholders } = require("../controllers/templates.controller");
-
-router.get(
-  "/api/v1/business/templates/:id/placeholders",
-  authenticate, // Bearer authentication middleware
-  authorize("business_owner"), // Business owner authorization middleware
-  getTemplatePlaceholders
-);
 
 module.exports = router;
