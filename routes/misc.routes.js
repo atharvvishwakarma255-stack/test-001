457f525cc8a6d3cd59006583085a6b598881de93
AniketVishwakarma7575 <aniketv123456ishwakarma@gmail.com>
2026-07-28 18:32:55 +0530

    test 0032

--- routes/misc.routes.js
+++ routes/misc.routes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { healthCheck, usersList, sendEmail, syncDocs, testSync, sendInvoiceReminder } = require("../controllers/misc.controller");
+const { healthCheck, usersList, sendEmail, syncDocs, sendInvoiceReminder } = require("../controllers/misc.controller");
 
 
 router.get("/health", healthCheck);
 router.get("/users", usersList);
 router.post("/send-email", sendEmail);
 router.post("/sync-docs", syncDocs);
-router.post("/test-sync", testSync);
 router.post("/send-invoice-reminder/:invoiceId", sendInvoiceReminder); 
 
 
